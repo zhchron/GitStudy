bc53924b4b6eabae82ef0516d7cbf0afc6ba60c6
zhchron <zhchron@163.com>
2020-12-12 11:08:28 +0800

    加个问号

--- src/main/java/com/zhchron/gittest/Hello.java
+++ src/main/java/com/zhchron/gittest/Hello.java
@@ -2,6 +2,6 @@ package com.zhchron.gittest;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello wang 提交");
     }
 }
