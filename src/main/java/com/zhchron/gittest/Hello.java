e596c24875e2aa0d150d5aca09c341e9e5dc739e
zhchron <zhchron@163.com>
2020-12-12 11:03:12 +0800

    加个问号

--- src/main/java/com/zhchron/gittest/Hello.java
+++ src/main/java/com/zhchron/gittest/Hello.java
@@ -2,6 +2,6 @@ package com.zhchron.gittest;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Conflict");
+        System.out.println("Hello Conflict？");
     }
 }
